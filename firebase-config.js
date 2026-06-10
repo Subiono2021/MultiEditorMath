7ee14559c5c53945acd514e02bfd377118265009
BiMath <91346750+Subiono2021@users.noreply.github.com>
2026-06-10 16:01:41 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyB1SQF1GnLy45Eu7CIYmMJnnQsuHkia-ts",
+  authDomain: "multi-editor-labs.firebaseapp.com",
+  projectId: "multi-editor-labs",
+  storageBucket: "multi-editor-labs.firebasestorage.app",
+  messagingSenderId: "773652810417",
+  appId: "1:773652810417:web:ec88f1b0f9a7da5d58f090",
+  measurementId: "G-4K0RLZDJJH"
+};
